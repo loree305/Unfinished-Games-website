ac85b12b89fede20a08667b6fd591b0380369633
loree305 <60853829+loree305@users.noreply.github.com>
2024-01-18 14:27:35 -0500

    Add files via upload

---
+++ Boggle.js
@@ -0,0 +1,22 @@
+function words(x)
+{
+	switch (x)
+	{
+	case 1:
+		var word = new Array("balte","table", "blate", "teal", "tael", "blet", "blae", "belt", "bate", "able", "tel", "tae", "let", "lat", "eta", "bet", "bat", "ate", "ale", "bleat", "tela", "tale", "late", "blat", "beta", "beat", "bale", "abet", "tea", "tab", "lea", "lab", "eat", "bel", "bal", "alt");
+		break;		
+	case 2:
+		var word = new Array("atwre","water", "wert", "wart,", "twae", "tare", "wet", "war", "twa", "tea", "tar", "ret", "rat", "era", "ear", "ate", "are", "tawer", "wear", "ware", "tear", "rate", "wat", "wae", "tew", "taw", "tae", "raw", "eta", "eat", "awe", "art");
+		break;
+	case 3:
+		var word = new Array("dclaen","lanced", "naled", "laden", "eland", "dance", "clade", "acned", "lean", "lane", "lade", "elan", "deal", "dace", "clad", "cade", "acne", "nae", "lea", "lac", "eld", "del", "cel", "cad", "and", "ace", "candle", "lance", "laced", "decal", "clean", "caned", "lend", "lead", "land", "lace", "dean", "dale", "clan", "cane", "alec", "aced", "led", "lad", "end", "den", "dal", "can", "ane", "ale");
+		break;
+	case 4:
+		var word = new Array("aepinlar","airplane", "praline", "replan", "realia", "planer", "pirana", "penial", "panier", "narial", "linear", "earlap", "alpine", "aerial", "repin", "plier", "plane", "pilea", "peril", "pearl", "panel", "palea", "naira", "liane", "learn", "lanai", "elain", "ariel", "areal", "apian", "anear", "alien", "ripe", "riel", "rein", "real", "rani", "rain", "raia", "plea", "pirn", "pina", "pier", "pial", "pein", "pean", "pare", "pane", "pair", "pail", "near", "nape", "lire", "lipa", "lier", "liar", "leap", "lari", "lair", "ilea", "earn", "aril", "area", "anil", "alar", "alae", "rip", "ria", "rei", "ran", "pin", "pia", "pen", "par", "pal", "nil", "nae", "lin", "lei", "lar", "ire", "era", "are", "ani", "ana", "ale", "air", "ail", "preanal", "plainer", "renail", "rapine", "planar", "pineal", "parian", "palier", "nailer", "larine", "apneal", "aliner", "ripen", "renal", "plena", "plain", "pilar", "penal", "parle", "paler", "paean", "liner", "liana", "lapin", "laari", "arpen", "arena", "apnea", "anile", "aline", "alane", "rile", "rial", "reap", "rape", "rale", "rail", "plie", "plan", "pine", "pile", "pian", "peri", "pear", "peal", "para", "pale", "pain", "nipa", "neap", "nail", "lira", "line", "lien", "lear", "lean", "lane", "lain", "elan", "earl", "aria", "aper", "anal", "alan", "airn", "rin", "rep", "rap", "rai", "pie", "per", "pea", "pan", "nip", "nap", "lip", "lie", "lea", "lap", "ern", "ear", "ape", "ane", "alp", "ala", "ain");
+		break;
+	case 5:
+		var word = new Array("redykboa","keyboard", "rebody", "kayoed", "byroad", "brayed", "boyard", "bakery", "yerba", "robed", "rayed", "oread", "oared", "drake", "dobra", "deray", "deary", "broad", "bread", "brake", "bored", "beard", "beady", "barye", "bared", "baker", "ardeb", "adobe", "yore", "yerk", "yare", "ryke", "robe", "redo", "rake", "orby", "okra", "odea", "kyar", "kora", "kbar", "eyra", "dyer", "dray", "dory", "dore", "doby", "dark", "darb", "bred", "brae", "bork", "bora", "bode", "berk", "beak", "bark", "bard", "bade", "aero", "abed", "yod", "yea", "yak", "rya", "rod", "red", "ray", "ore", "ora", "oka", "oda", "oba", "oak", "kor", "koa", "ked", "kay", "kab", "ear", "dry", "doe", "deb", "dak", "bye", "bra", "bod", "bey", "bay", "bad", "ark", "arb", "redbay", "okayed", "debark", "bready", "braked", "barked", "yoked", "ryked", "ready", "raked", "orbed", "kebar", "dorky", "derby", "debar", "broke", "break", "braky", "boyar", "board", "beaky", "bayed", "barky", "barde", "baked", "adore", "abode", "yoke", "year", "yard", "rode", "road", "read", "oyer", "orad", "okay", "obey", "kore", "kerb", "kayo", "dyke", "drek", "drab", "dork", "doer", "dear", "dare", "byre", "bray", "brad", "bore", "body", "boar", "bear", "bead", "bare", "bake", "aery", "abye", "yok", "yob", "yar", "rye", "roe", "rob", "reb", "rad", "orb", "oke", "ode", "obe", "oar", "kye", "kob", "key", "kea", "kae", "era", "dye", "dor", "dey", "day", "dab", "bro", "boy", "boa", "bed", "bar", "aye", "are", "ado")
+		break;
+	}
+	return word;
+}
